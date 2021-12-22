aae8e23f5575313f18b9c2fb1205764ef8a0e506
Nicole Freißmuth <freissmuthnicole@gmx.at>
2021-12-22 09:18:30 +0100

    serverlein.py

--- serverlein.py
+++ serverlein.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python3
 
+#Freißmuth Nicole
+
+import socket
+
+HOST = ''
+PORT = 8080
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Fehlermeldung verhindern
+    s.bind((HOST, PORT))
+    s.listen(1)
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            data = conn.recv(1024)
+            #if not data: break
+            outdata = "HTTP/1.1 200 OK\r\n\r\n<html><body><h1>Hi BULME</h1></body></html>"
+            antwort = outdata.encode()
+            #print('Connected by', addr)
+            print("Serverantwort:", antwort, "an", addr)
+            #print(data)
+            conn.sendall(antwort)
+            conn.close()
+      
